e48f1578153a7971a8629eb746860221a3d139f9
yubmun <dlwnsduq23@gmail.com>
2023-02-23 11:44:50 +0900

    함수에 타입 지정하기

--- ch4/ch4.js
+++ ch4/ch4.js
@@ -48,3 +48,51 @@ function 함수6(income, haveHouse, score) {
 console.log(함수6(700, false, "중"));
 console.log(함수6(100, false, "상"));
 console.log(함수6(100, true, "상"));
+/**
+ * 이름을 파라미터로 입력하면 콘솔창에 안녕하세요 홍길동 을 출력해주고 아무것도 입력하지 않으면 이름이 없습니다.
+ */
+function 숙제1(x) {
+    if (typeof x === "string") {
+        console.log("안녕하세요" + x);
+    }
+    else {
+        console.log("이름이 없습니다");
+    }
+}
+console.log(숙제1("이준엽"));
+console.log(숙제1());
+/**
+ * 함수에 숫자 또는 문자를 집어넣으면 자릿수를 세어주는 함수를 만들자
+ */
+function 숙제2(x) {
+    if (typeof x === "string") {
+        return x.length;
+    }
+    if (typeof x === "number") {
+        var result = x.toString();
+        return result.length;
+    }
+}
+console.log(숙제2('245'));
+console.log(숙제2(9568));
+/**
+ * 함수의 파라미터로 월소득, 집보유여부, 매력점수 를 입력
+ * 월소득은 1만원당 1점, 집보유시 500점, 미보유 0점, 매력점수는 상일때만 100점으로 계산
+ * 총 점수가 600점 이상일 때 결혼가능을 return 그 외엔 아무것도 return 하지 않음
+ */
+function 숙제3(x, y, z) {
+    var result = 0;
+    result += x;
+    if (y) {
+        result += 500;
+    }
+    if (z === "상") {
+        result += 100;
+    }
+    if (result > 600) {
+        return "결혼가능";
+    }
+}
+console.log(숙제3(700, false, "중"));
+console.log(숙제3(100, false, "상"));
+console.log(숙제3(300, true, "상"));
