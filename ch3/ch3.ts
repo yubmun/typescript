8959ae7d35b87d79b60cf6f599a967756a654ae4
yubmun <dlwnsduq23@gmail.com>
2023-02-23 10:56:16 +0900

    union type, any, unknown

--- ch3/ch3.ts
+++ ch3/ch3.ts
@@ -1,24 +1,28 @@
-// 문자 혹은 숫자가 들어올 수 있는 변수는? or연산자로 써서 쓰는걸 Union 타입이라고 한다.
+// 문자 혹은 숫자가 들어올 수 있는 변수는? | or연산자로 써서 쓰는걸 Union 타입이라고 한다.
 let 둘다:number | string = "kim";
 let 셋다:(number|string|boolean) = true; // () 써주는건 자유
 
 둘다 = 123;
 셋다 = "셋다된다";
 
+// 잠깐 숙제
 let 어레이:(string|number)[] = [1,'2',3]; // number | string[] 이건 1234, ["kim","lee"]이런거임
-let 오브젝:{a:string|number} = { a : "123"};
+let 오브젝:{ a:string|number } = { a : "123"};
 
-let 애니:any; // 모든 자료형을 허용해준다. 타스를 쓰는 이유가 없어진다. 
+let 애니:any; // 모든 자료형을 허용해준다. 타입스크립트를 쓰는 이유가 없어진다. 
 애니 = 123;
 애니 = true;
 
 let 언노운:unknown; // any와 매우 유사한 타입, 용도도 똑같음 이름만 다름. 조금더 안전하다. 왜? 언노운은 number 타입도 아니라서 연산이 안된다. 
 
-// let 변수1:string = 언노운; 이건 안되지만 애니는 할당이 되는게 문제임 애니는 바이러스같은 존재
+// let 변수1:string = 언노운; 이건 안되지만 any는 할당이 되는게 문제임 any는 바이러스같은 존재
 
 let age:string|number;
 //age + 1; 타스는 엄격해서 이거 계산 안됨
+// 자바스크립트에서는 age + 1 , 문자도 숫자도 + 연산이 되기 때문에 union 타입에는 적용이 안된다.
 
+let 나이:unknown = 1;
+// 나이 - 1 언노운도 확실한 타입이 아니기 때문에 사칙연산이 불가하다.
 
 // 문제1
 let user:string = "kim";
