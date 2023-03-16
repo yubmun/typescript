ec77e300ee0a5fd4374e3a91015c961e1fab40a3
yubmun <dlwnsduq23@gmail.com>
2023-03-16 11:06:11 +0900

    keyof 로 타입변환기

--- ch26/ch26.ts
+++ ch26/ch26.ts
@@ -25,6 +25,41 @@ type TypeChanger<MyType> = {
 }
 type NewCar = TypeChanger<Car>; // Car 에 있는 모든 타입이 NewCar에선 모두 string 타입으로 변환되었다.
 
+/**
+ * 숙제1) 다음 타입을 변환기를 돌려보십시오.
+type Bus = {
+  color : string,
+  model : boolean,
+  price : number
+}
+동료가 잘못 만든 타입입니다.
+color, model, price 속성은 전부 string 또는 number 타입이어야합니다.
+
+1. 변환기 하나 만드시고
+
+2. 기존 Bus 타입을 변환기 돌려서 위 조건을 충족하는 새로운 타입을 하나 만들어보십시오.
+ */
+
+type TypeChanger2<Bus> = {
+  [key in keyof Bus]: string | number;
+}
+type newType = TypeChanger2<Bus>;
+
+/**
+ * (숙제2) 이런 변환기는 어떻게 만들어야할까요?
+
+object안에 들어있는 모든 속성을
+
+string, number 이렇게 고정된 타입으로 변환해주는게 아니라
+
+내가 원하는 타입을 입력하면 그걸로 변환해주는 범용성 좋은 변환기를 만들어보십시오.
+ */
+
+type TypeChanger3<Bus, T> = {
+  [key in keyof Bus]: T;
+}
+type NewType2 = TypeChanger3<Bus, string>;
+
 //숙제1 
 type Bus = {
   color: string,
