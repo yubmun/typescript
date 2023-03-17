969b58b85b433c3d742d249017197e6d0573e985
yubmun <dlwnsduq23@gmail.com>
2023-03-17 12:43:38 +0900

    test 연습

--- ch27/ch27.ts
+++ ch27/ch27.ts
@@ -6,6 +6,54 @@ type Age2<T> = T extends string ? string : unknown;
 
 let a2:Age2<number>; // unknown
 
+// 중간 퀴즈
+// 파라미터로 array가 들어오면 array의 첫 index 자료형을 남김 , array로 안들어오면 any 타입 남김
+
+type hw1<T> = T extends any[] ? T[0] : any;
+type Check = hw1<string[]>;
+// 이게 최종 이었음, any 나와버리는 꼬라지..
+
+/**
+ * 숙제1
+ * (숙제1) 타입 파라미터로 
+
+1. array 타입을 입력하면
+
+2. array의 첫 자료가 string이면 string 타입을 그대로 남겨주고 
+
+3. array의 첫 자료가 string이 아니면 unknown 을 남겨주려면 어떻게 타입을 만들어놔야할까요?
+(동작예시)
+
+let age1 :Age<[string, number]>;
+let age2 :Age<[boolean, number]>; 
+이러면 age1의 타입은 string, age2의 타입은 unknown이 되어야합니다. (array나 tuple이나 그게 그거임)
+
+이걸 만족하는 type Age를 만들어봅시다.
+ */
+type Age1<T> = T extends [string, any] ? string : unknown;
+
+let age4 :Age1<[string, number]>;
+let age5 :Age1<[boolean, number]>;
+
+/**
+ * 숙제2) 함수의 파라미터의 타입을 뽑아주는 기계를 만들어보십시오. 
+
+타입뽑기<(x :number) => void> //이러면 number가 이 자리에 남음
+타입뽑기<(x :string) => void> //이러면 string이 이 자리에 남음
+아무튼 함수의 파라미터타입이 남아야합니다.
+ */
+
+type Machine<T> = T extends (x: infer R) => void ? R : unknown;
+
+type hw11 = Machine<(x: number) => void>;
+type hw12 = Machine<(x: string) => void>;
+
+
+
+
+
+
+
 type FirstItem<T> = T extends any[] ? T[0] : any;
 let age1:FirstItem<string[]>
 let age2:FirstItem<number>
